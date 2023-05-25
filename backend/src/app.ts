5ca40c2b176318572d6dc2d8ebb1347fc1cb74fd
Pietro Piva Vieira <pietro.developer@gmail.com>
2023-05-25 17:54:31 -0300

    feat: add backend with routes and ws events

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,51 @@
 import { fastify } from 'fastify'
-import { socketioServer as socketServer } from 'fastify-socket.io'
+import fastifySocketIO from 'fastify-socket.io'
 
+import { globalHttpErrorHandler } from './globals/globalHttpErrorHandler'
 import { newGlobalMessageSchemaValidator } from './events/validators/new-global-message-schema-validator'
+import { appRoutes } from './routes'
+
+const userMap = new Map<string, string>()
+const typingUsersSet = new Set<string>()
 
 export const app = fastify()
-app.register(socketServer, {
+app.register(fastifySocketIO, {
   transports: ['websocket'],
 })
+app.register(appRoutes)
+
+app.setErrorHandler(globalHttpErrorHandler)
 
 app.ready().then(() => {
+  app.io.use((socket, next) => {
+    if (!userMap.has(socket.id)) {
+      userMap.set(socket.id, String(socket.handshake.query.nickname))
+    }
+
+    next()
+  })
+
   app.io.on('connection', (socket) => {
-    console.log('User connected', socket.id)
+    socket.on('global.user.typing', () => {
+      const typingUserUsername = userMap.get(socket.id)
+      typingUsersSet.add(socket.id)
+
+      if (typingUsersSet.size > 0) {
+        app.io.emit('global.user.typing', 'Muita gente está digitando...')
+        return
+      }
+
+      if (!typingUserUsername) {
+        app.io.emit('global.user.typing', 'Um usuário está digitando...')
+        return
+      }
+
+      app.io.emit('global.user.typing', `${typingUserUsername} está digitando`)
+    })
+
+    socket.on('global.user.typing', () => {
+      typingUsersSet.delete(socket.id)
+    })
 
     socket.on('global.message.new', (payload: any) => {
       const messagePayload = newGlobalMessageSchemaValidator.parse(payload)
@@ -19,7 +54,18 @@ app.ready().then(() => {
     })
 
     socket.on('disconnect', (_) => {
-      console.log('User disconnected')
+      const disconnectedUserUsername = userMap.get(socket.id)
+      userMap.delete(socket.id)
+
+      if (!disconnectedUserUsername) {
+        app.io.emit('global.disconnection', 'Um usuário se desconectou')
+        return
+      }
+
+      app.io.emit(
+        'global.disconnection',
+        `${disconnectedUserUsername} se desconectou`,
+      )
     })
   })
 })
---
+++ backend/src/routes.ts
@@ -0,0 +1,67 @@
+import { FastifyInstance } from 'fastify'
+import { compare, hash } from 'bcryptjs'
+import { z } from 'zod'
+
+import { prisma } from './lib/prisma'
+
+const createUserBodySchemaValidator = z.object({
+  nickname: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+const authenticateUserBodySchemaValidator = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/users/create', async (request, reply) => {
+    const { nickname, email, password } = createUserBodySchemaValidator.parse(
+      request.body,
+    )
+
+    const passwordHash = await hash(password, 6)
+
+    await prisma.user.create({
+      data: {
+        nickname,
+        email,
+        password_hash: passwordHash,
+      },
+    })
+
+    return reply.status(201).send()
+  })
+  app.post('/users/authenticate', async (request, reply) => {
+    const { email, password } = authenticateUserBodySchemaValidator.parse(
+      request.body,
+    )
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user) {
+      return reply.status(400).send({
+        error: true,
+        message: 'Email ou Senha incorretos!',
+      })
+    }
+
+    const passwordIsValid = await compare(password, user.password_hash)
+
+    if (!passwordIsValid) {
+      return reply.status(400).send({
+        error: true,
+        message: 'Email ou Senha incorretos!',
+      })
+    }
+
+    return reply.status(200).send({
+      valid: true,
+    })
+  })
+}
